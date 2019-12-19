7fac33e4e48ac7e48a782e52b57f230aef70a75e
Sejin Kim <kim3@kenyon.edu>
2019-12-18 23:49:22 -0500

    Big updates, r code has been refactored.

--- scripts/evalscript.R
+++ scripts/evalscript.R
@@ -3,15 +3,22 @@
 # author: S Kim
 #################################################
 
+## For all tests, will use alpha = .05
+## For all model building, will use alpha = .1
+
 #################################################
 ## do prelim work
 ## load packages
 library(mosaic)
-library(Stat2Data)
-library(leaps)
+library(PerformanceAnalytics)
+library(psych)
 library(Hmisc)
 library(HH)
 library(caret)
+library(car)
+library(rcompanion)
+library(dplyr)
+library(lmtest)
   ## Hmisc is for numerical correlation matrix
 
 #################################################
@@ -25,15 +32,27 @@ head(nhanes)
 #################################################
 ## some EDA
 tally(~SurveyYr, data = nhanes)
+favstats(~Diabetes, data = nhanes)
 ncol(nhanes)
+## find correlations
+# load numerical data into matrix
+nummat <- nhanes[, c("Age","HHIncomeMid","HomeRooms","Weight","Height",
+                     "BMI","Pulse","BPSysAve","BPDiaAve","DirectChol",
+                     "TotChol", "UrineVol1","UrineFlow1","Diabetes","DaysPhysHlthBad",
+                     "DaysMentHlthBad","SleepHrsNight","PhysActiveDays","AlcoholDay","AlcoholYear")]
+mat1 <- rcorr(as.matrix(nummat))
+mat1
+chart.Correlation(nummat, histogram = TRUE, pch=16)
+corr.test(nummat, use = "pairwise", adjust="none", alpha=0.05)
+
 ## make some scatter or box plots
-plot(Diabetes~Gender, data = nhanes)
+plot(Diabetes~Gender, main = "Diabetes by Gender", data = nhanes)
 plot(Diabetes~Age, data = nhanes)
-plot(Diabetes~Race1, data = nhanes)
+plot(Diabetes~Race1, main = "Diabetes by Race", xlab = "Race", data = nhanes)
 plot(Diabetes~HHIncomeMid, data = nhanes)
 plot(Diabetes~HomeRooms, data = nhanes)
 plot(Diabetes~HomeOwn, data = nhanes)
-plot(Diabetes~Work, data = nhanes)
+plot(Diabetes~Work, main = "Diabetes by Work Status", xlab = "Work Status", data = nhanes)
 plot(Diabetes~BMI, data = nhanes)
 plot(Diabetes~Pulse, data = nhanes)
 plot(Diabetes~BPSysAve, data = nhanes)
@@ -42,212 +61,162 @@ plot(Diabetes~DirectChol, data = nhanes)
 plot(Diabetes~TotChol, data = nhanes)
 plot(Diabetes~UrineVol1, data = nhanes)
 plot(Diabetes~UrineFlow1, data = nhanes)
-plot(Diabetes~HealthGen, data = nhanes)
+plot(Diabetes~HealthGen, main = "Diabetes by General Health Status", xlab = "General Health", data = nhanes)
 plot(Diabetes~PhysActive, data = nhanes)
 plot(Diabetes~AlcoholDay, data = nhanes)
-## find correlations
-# load numerical data into matrix
-nummat <- nhanes[, c("Age",
-                     "HHIncomeMid",
-                     "HomeRooms",
-                     "Weight",
-                     "Height",
-                     "BMI",
-                     "Pulse",
-                     "BPSysAve",
-                     "BPDiaAve",
-                     "DirectChol",
-                     "TotChol",
-                     "UrineVol1",
-                     "UrineFlow1",
-                     "Diabetes",
-                     "DaysPhysHlthBad",
-                     "DaysMentHlthBad",
-                     "SleepHrsNight",
-                     "PhysActiveDays",
-                     "AlcoholDay",
-                     "AlcoholYear")]
-mat1 <- rcorr(as.matrix(nummat))
-mat1
 
 #################################################
 ## Try building some models
-## automated backwards procedure
-fullback <- lm(Diabetes~Gender+
-                 Age+
-                 Race1+
-                 HHIncomeMid+
-                 HomeRooms+
-                 HomeOwn+
-                 Work+
-                 Weight+
-                 Height+
-                 BMI+
-                 Pulse+
-                 BPSysAve+
-                 BPDiaAve+
-                 DirectChol+
-                 TotChol+
-                 UrineVol1+
-                 UrineFlow1+
-                 HealthGen+
-                 DaysPhysHlthBad+
-                 DaysMentHlthBad+
-                 SleepHrsNight+
-                 PhysActive+
-                 PhysActiveDays+
-                 AlcoholDay+
-                 AlcoholYear,
-               data = nhanes)
-MSE = (summary(fullback)$sigma)^2
-step(fullback, scale = MSE, direction = "backward")
-
-## automated forwards procedure
-noneforw <- lm(Diabetes~1, data = nhanes)
-step(noneforw, scope = list(upper=fullback), scale = MSE, direction = "forward")
-
-## automated stepwise procedure
-nonestep <- lm(Diabetes~1, data = nhanes)
-step(nonestep, scope = list(upper=fullback), scale = MSE)
-
-## best subsets procedure
-all = regsubsets(Diabetes~Gender+
-                   Age+
-                   Race1+
-                   HHIncomeMid+
-                   HomeRooms+
-                   HomeOwn+
-                   Work+
-                   Weight+
-                   Height+
-                   BMI+
-                   Pulse+
-                   BPSysAve+
-                   BPDiaAve+
-                   DirectChol+
-                   TotChol+
-                   UrineVol1+
-                   UrineFlow1+
-                   HealthGen+
-                   DaysPhysHlthBad+
-                   DaysMentHlthBad+
-                   SleepHrsNight+
-                   PhysActive+
-                   PhysActiveDays+
-                   AlcoholDay+
-                   AlcoholYear,
-                 data = nhanes)
-  ## explore adjusted R^2 values
-summary(all)$adjr2
-plot(all, scale = "adjr2")
-  ## restricted subset
-newrestrict = regsubsets(Diabetes~Gender+
-                           Age+
-                           Race1+
-                           HHIncomeMid+
-                           HomeRooms+
-                           HomeOwn+
-                           Work+
-                           Weight+
-                           Height+
-                           BMI+
-                           Pulse+
-                           BPSysAve+
-                           BPDiaAve+
-                           DirectChol+
-                           TotChol+
-                           UrineVol1+
-                           UrineFlow1+
-                           HealthGen+
-                           DaysPhysHlthBad+
-                           DaysMentHlthBad+
-                           SleepHrsNight+
-                           PhysActive+
-                           PhysActiveDays+
-                           AlcoholDay+
-                           AlcoholYear,
-                         nbest = 2,
-                         data = nhanes)
-summary(newrestrict)
-  ## explore Cp values, smaller is better
-plot(newrestrict, scale = "Cp")
-  ## get the Cp for any model
-newfull = lm(Diabetes~Gender+
-               Age+
-               Race1+
-               HHIncomeMid+
-               HomeRooms+
-               HomeOwn+
-               Work+
-               Weight+
-               Height+
-               BMI+
-               Pulse+
-               BPSysAve+
-               BPDiaAve+
-               DirectChol+
-               TotChol+
-               UrineVol1+
-               UrineFlow1+
-               HealthGen+
-               DaysPhysHlthBad+
-               DaysMentHlthBad+
-               SleepHrsNight+
-               PhysActive+
-               PhysActiveDays+
-               AlcoholDay+
-               AlcoholYear,
-             data = nhanes)
-MSE=(summary(newfull)$sigma)^2
-extractAIC(lm(Diabetes~Gender+
-                Age+
-                Race1+
-                HHIncomeMid+
-                HomeRooms+
-                HomeOwn+
-                Work+
-                Weight+
-                Height+
-                BMI+
-                Pulse+
-                BPSysAve+
-                BPDiaAve+
-                DirectChol+
-                TotChol+
-                UrineVol1+
-                UrineFlow1+
-                HealthGen+
-                DaysPhysHlthBad+
-                DaysMentHlthBad+
-                SleepHrsNight+
-                PhysActive+
-                PhysActiveDays+
-                AlcoholDay+
-                AlcoholYear,
-              data = nhanes),
-           scale = MSE)
-summaryHH(all)
-
-# best model uses:
-## Age, HomeOwnOwn, BMI, Pulse, TotChol, HealthGen, DaysPhysHlthBad, DaysMentHlthBad
+#################################################
+## Record influences at the univariate level as a first round elimination
+glmgend <- glm(Diabetes~Gender, data = nhanes)
+summary(glmgend) #**
+glmage <- glm(Diabetes~Age, data = nhanes)
+summary(glmage) #***
+glmrace <- glm(Diabetes~Race1, data = nhanes)
+summary(glmrace) #NOT SIGNIFICANT - ELIMINATE
+glmhhin <- glm(Diabetes~HHIncomeMid, data = nhanes)
+summary(glmhhin) #**
+glmhroo <- glm(Diabetes~HomeRooms, data = nhanes)
+summary(glmhroo) #NOT SIGNIFICANT - ELIMINATE
+glmhown <- glm(Diabetes~HomeOwn, data = nhanes)
+summary(glmhown) #NOT SIGNIFICANT - ELIMINATE
+glmwork <- glm(Diabetes~Work, data = nhanes)
+summary(glmwork) #Not working is significant at *, consider eliminating
+glmweig <- glm(Diabetes~Weight, data = nhanes)
+summary(glmweig) #***
+glmheig <- glm(Diabetes~Height, data = nhanes)
+summary(glmheig) # NOT SIGNIFICANT - ELIMINATE
+glmbmi <- glm(Diabetes~BMI, data = nhanes)
+summary(glmbmi) #***
+glmpuls <- glm(Diabetes~Pulse, data = nhanes)
+summary(glmpuls) #NOT SIGNIFICANT - ELIMINATE
+glmbpsy <- glm(Diabetes~BPSysAve, data = nhanes)
+summary(glmbpsy) #***
+glmbpdi <- glm(Diabetes~BPDiaAve, data = nhanes)
+summary(glmbpdi) #NOT SIGNIFICANT - ELIMINATE
+glmdire <- glm(Diabetes~DirectChol, data = nhanes)
+summary(glmdire) #***
+glmtotc <- glm(Diabetes~TotChol, data = nhanes)
+summary(glmtotc) #***
+glmuvol <- glm(Diabetes~UrineVol1, data = nhanes)
+summary(glmuvol) #***
+glmuflo <- glm(Diabetes~UrineFlow1, data = nhanes)
+summary(glmuflo) #**
+glmheal <- glm(Diabetes~HealthGen, data = nhanes)
+summary(glmheal) #Only fair and good are significant at *, consider eliminating
+glmdphy <- glm(Diabetes~DaysPhysHlthBad, data =nhanes)
+summary(glmdphy) #***
+glmdmen <- glm(Diabetes~DaysMentHlthBad, data = nhanes)
+summary(glmdmen) #**
+glmslee <- glm(Diabetes~SleepHrsNight, data = nhanes)
+summary(glmslee) #NOT SIGNIFICANT - ELIMINATE
+glmphys <- glm(Diabetes~PhysActive, data = nhanes)
+summary(glmphys) #***
+glmpday <- glm(Diabetes~PhysActiveDays, data = nhanes)
+summary(glmpday) #**
+glmalco <- glm(Diabetes~AlcoholDay, data = nhanes)
+summary(glmalco) #NOT SIGNIFICANT - ELIMINATE
+glmalcy <- glm(Diabetes~AlcoholYear, data = nhanes)
+summary(glmalcy) #NOT SIGNIFICANT - ELIMINATE
+##
+## KEEP Gender, Age, HHIncomeMid, Work, Wieght, BMI, BPSysAve, DirectChol, TotChol, UrineVol1, UrineFlow1,
+## HealthGen, DaysPhysHlthBad, DaysMentHlthBad, PhysActive, PhysActiveDays
 
 #################################################
-## Validation by training and holding out
-favstats(~Diabetes, data = nhanes)[c("n")]
-(trainn <- 0.67*2167) # 1452
-(holdn <- 0.33*2167) # 715
-## break the dataset
-trainnhanes <- nhanes[c(1:1452),]
-holdoutnhanes <- nhanes[c(1453:2167),]
-## train the model
-diabetestrainmod <- glm(Diabetes~Age+HomeOwn+BMI+Pulse+TotChol+HealthGen+DaysPhysHlthBad+DaysMentHlthBad, data = trainnhanes)
-summary(diabetestrainmod)
-predictions <- diabetestrainmod %>% predict(holdoutnhanes)
-predictions
-diabeteshat <- predict(diabetestrainmod, holdoutnhanes)
-residual = holdoutnhanes$Diabetes - diabeteshat
-residual
-summary(residual)
-mean(residual)
-sd(residual)
-cor(holdoutnhanes$Diabetes~diabeteshat)
+## run the actual logitic regression
+## define full and null models and do the stepwise procedure
+model.null = glm(Diabetes ~ 1,
+                 data = nhanes,
+                 family = binomial(link="logit"))
+model.full = glm(Diabetes ~ Gender + Age + HHIncomeMid + Work + Weight +
+                   BMI + BPSysAve + DirectChol + TotChol + UrineVol1 + 
+                   UrineFlow1 + HealthGen + DaysPhysHlthBad + DaysMentHlthBad +
+                   PhysActive + PhysActiveDays,
+                 data = nhanes,
+                 family = binomial(link="logit"))
+step(model.null,
+     scope = list(upper = model.full),
+     direction = "both",
+     test="Chisq",
+     data = nhanes)
+## test built model
+model.next = glm(Diabetes ~ Age + BMI + TotChol + DaysPhysHlthBad +
+                    HealthGen + UrineVol1 + DirectChol + DaysMentHlthBad +
+                    Work + PhysActiveDays + BPSysAve,
+                  data = nhanes,
+                  family = binomial(link="logit"),
+                  na.action(na.omit))
+summary(model.next)
+## test model with insigificant terms removed
+model.final = glm(Diabetes ~ Age + BMI + TotChol + DaysPhysHlthBad + 
+                    HealthGen + UrineVol1 + DirectChol + DaysMentHlthBad +
+                    Work,
+                  data = nhanes,
+                  family = binomial(link="logit"),
+                  na.action(na.omit))
+summary(model.final)
+## analysis of variance for individual terms - uses lib(car)
+Anova(model.final, type = "II", test = "Wald")
+## pseudo-R-squared
+nagelkerke(model.final)
+##overall p-value for the model
+#### create data frame with variables in final model and NA's omitted
+Data.final = select(nhanes,
+                    Diabetes, 
+                    Age, 
+                    BMI, 
+                    TotChol, 
+                    DaysPhysHlthBad, 
+                    HealthGen, 
+                    UrineVol1, 
+                    DirectChol, 
+                    DaysMentHlthBad, 
+                    Work)
+Data.final = na.omit(Data.final)
+#### define null models and compare to the final model
+new.model.null <- glm(Diabetes~1,
+                  data = Data.final,
+                  family = binomial(link="logit")
+                  )
+anova(model.final, new.model.null, test="Chisq")
+lrtest(model.final)
+## plot of standardized residuals
+plot(fitted(model.final), rstandard(model.final))
+## simple plot of predicted values
+plot(Diabetes ~ Age,
+     data = Data.final,
+     pch = 16,
+     xlab = "Predicted probability of 1 response",
+     ylab = "Actual response",
+     main = "Diabetes ~ Age Predicted Values Plot")
+## check for overdispersion
+summary(model.final)$deviance / summary(model.final)$df.residual
+  ## ratio is < 1.5, so no worries about overdispersion
+
+#################################################
+## Alternative (or double-checking) the model assessement using compareGLM
+#### define models to compare
+model.1 <- glm(Diabetes~1,
+               data = Data.final, family=binomial())
+model.2 <- glm(Diabetes ~ Age,
+               data = Data.final, family=binomial())
+model.3 <- glm(Diabetes ~ Age + BMI,
+               data = Data.final, family=binomial())
+model.4 <- glm(Diabetes ~ Age + BMI + TotChol,
+               data = Data.final, family=binomial())
+model.5 <- glm(Diabetes ~ Age + BMI + TotChol + DaysPhysHlthBad,
+               data = Data.final, family=binomial())
+model.6 <- glm(Diabetes ~ Age + BMI + TotChol + DaysPhysHlthBad + HealthGen,
+               data = Data.final, family=binomial())
+model.7 <- glm(Diabetes ~ Age + BMI + TotChol + DaysPhysHlthBad + HealthGen + UrineVol1,
+               data = Data.final, family=binomial())
+model.8 <- glm(Diabetes ~ Age + BMI + TotChol + DaysPhysHlthBad + HealthGen + UrineVol1 + DirectChol,
+               data = Data.final, family=binomial())
+model.9 <- glm(Diabetes ~ Age + BMI + TotChol + DaysPhysHlthBad + HealthGen + UrineVol1 + DirectChol + DaysMentHlthBad,
+               data = Data.final, family=binomial())
+model.10 <- glm(Diabetes ~ Age + BMI + TotChol + DaysPhysHlthBad + HealthGen + UrineVol1 + DirectChol + DaysMentHlthBad + Work,
+                data = Data.final, family=binomial())
+
+#### use compare.glm to assess fit statistics
+anova(model.1, model.2, model.3, model.4, model.5, model.6, model.7, model.8, model.9, model.10, test="Chisq")
